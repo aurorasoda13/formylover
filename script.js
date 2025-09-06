36fc9234d93be4e143d1a98ebe462f81473a9a5e
aurorasoda13 <aurorasoda13@gmail.com>
2025-09-06 15:55:30 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -14,7 +14,12 @@ window.onload = function() {
     "Il nostro tempo non si può rubare\nFatto di tutto il meglio e il peggio di noi\nE sul mio viso c'è la tua espressione\nLe tue parole come io le direi\nLento, sento\nIl tuo respiro che si calma col mio\nUn altro inverno e stiamo ancora insieme\nUn altro freddo che non sento con te\n-Alessandra Amoroso",
     "Amore, stringimi, stringimi\nTienimi ancora così e\nScusami, scusami\nSe non sono riuscita mai\nNemmeno a dirti che\nNon m'importa niente, non m'importa della gente\nCiò che voglio veramente è solo stringerti di più\nIo non ho mai provato niente di così profondo per nessuno mai\nCon te mi sento come una bambina, una promessa, una bugia\nLa verità è che dovrei stringerti di più\nIo me ne accorgo solo adesso che ti amo come non ti ho amato mai\nCome mai\n-Giorgia",
     "Tu mi fai sentire un po' speciale\nAnche tra miliardi di persone\nSai che io ti riconoscerei\nCome una stella in mezzo a un cielo di cartone\nE dovessi perdermi, perdermi\nMille anni luce da qui\nAmore, stringimi, stringimi\nTienimi ancora così\n-Giorgia",
-  
+    "Sei il primo mio pensiero che\nAl mattino mi sveglia\nL'ultimo desiderio che\nLa notte mi culla\nSei la ragione più profonda\nDi ogni mio gesto\nLa storia più incredibile\nChe conosco\n-883",
+    "Il bisogno che ho di te\nNon si è mai potuto estinguere\nIl bisogno che ho di te\nSopravvive ai principi\n-Alessandra Amoroso",
+    "I'll make you happy, baby (ooh-ooh), just wait and see (ooh, ooh)\n(Ooh) for every kiss you give me (ooh-ooh), I'll give you three (ooh, ooh)\n(Ah) oh, since the day I saw you\n(Ah) I have been waiting for you\n(Ah) you know, I will adore you 'til eternity (ah)\nSo won't you, please (be my, be my baby)\nBe my little baby? (My one, and only baby)\nSay you'll be my darlin' (be my, be my baby)\nBe my baby now (my one, and only baby)\n-The Ronettes",
+    "I don't wanna look at anything else now that I saw you\nI don't wanna think of anything else now that I thought of you\nI've been sleepin' so long in a twenty-year dark night\nAnd now I see daylight, I only see daylight\n-Taylor Swift",
+    "ma in fondo gia lo so\nche io non ho, non ho, non ho\nnon ho che te...\n\nmi fai diventare la cosa più importante\nnon ho che te\na sorridermi per mano tra la gente\nnon ho che te\nche sai decifrare tutti i miei silenzi,\ncontrattempi del mio cuore\nsenza bisogno di spiegare\nnon ho che te\nanche se lo so, non ti dimostro niente\nnon ho che te\nil mio amore viaggia a velocità incostante\nnon ho che te\n-Alessandra Amoroso",
+
   ];
 
   const toggleInput = document.getElementById('toggle-treasure');
